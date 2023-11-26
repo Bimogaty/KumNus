ff8c560678f262d859eba014ef95cd1dae34519d
Bimogaty <happybirthday1157@gmail.com>
2023-11-26 22:46:50 +0700

    update

--- script1.js
+++ script1.js
@@ -1,99 +1,84 @@
 const questionPhotos = [
-    "photo/photo1.png",
-    "photo/photo2.png",
-    "photo/photo3.png",
-    "photo/photo4.png",
-    "photo/photo5.png",
-    "photo/photo6.png",
-    "photo/photo7.png",
-    "photo/photo8.png",
-    "photo/photo9.png",
-    "photo/photo10.png",
-    "photo/photo11.png",
-    "photo/photo12.png",
-    "photo/photo13.png",
-    "photo/photo14.png",
-    "photo/photo15.png",
-    "photo/photo16.png",
-    "photo/photo17.png",
-    "photo/photo18.png",
-    "photo/photo19.png",
-    "photo/photo20.png",
-    "photo/photo21.png",
-    "photo/photo22.png",
-    "photo/photo23.png",
-    "photo/photo24.png",
-    "photo/photo25.png",
-    "photo/photo26.png",
-    "photo/photo27.png",
-    "photo/photo28.png",
+    "photo/photo1.webp",
+    "photo/photo2.webp",
+    "photo/photo3.webp",
+    "photo/photo4.webp",
+    "photo/photo5.webp",
+    "photo/photo6.webp",
+    "photo/photo7.webp",
+    "photo/photo8.webp",
+    "photo/photo9.webp",
+    "photo/photo10.webp",
+    "photo/photo11.webp",
+    "photo/photo12.webp",
+    "photo/photo13.webp",
+    "photo/photo14.webp",
+    "photo/photo15.webp",
+    "photo/photo16.webp",
+    "photo/photo17.webp",
+    "photo/photo18.webp",
+    "photo/photo19.webp",
+    "photo/photo20.webp",
+    "photo/photo21.webp",
+    "photo/photo22.webp",
+    "photo/photo23.webp",
+    "photo/photo24.webp",
+    "photo/photo25.webp",
+    "photo/photo26.webp",
+    "photo/photo27.webp",
+    "photo/photo28.webp",
 ];
 
 const answerPhotos = [
-    "text/text1.png",
-    "text/text2.png",
-    "text/text3.png",
-    "text/text4.png",
-    "text/text5.png",
-    "text/text6.png",
-    "text/text7.png",
-    "text/text8.png",
-    "text/text9.png",
-    "text/text10.png",
-    "text/text11.png",
-    "text/text12.png",
-    "text/text13.png",
-    "text/text14.png",
-    "text/text15.png",
-    "text/text16.png",
-    "text/text17.png",
-    "text/text18.png",
-    "text/text19.png",
-    "text/text20.png",
-    "text/text21.png",
-    "text/text22.png",
-    "text/text23.png",
-    "text/text24.png",
-    "text/text25.png",
-    "text/text26.png",
-    "text/text27.png",
-    "text/text28.png",
+    "text/text1.webp",
+    "text/text2.webp",
+    "text/text3.webp",
+    "text/text4.webp",
+    "text/text5.webp",
+    "text/text6.webp",
+    "text/text7.webp",
+    "text/text8.webp",
+    "text/text9.webp",
+    "text/text10.webp",
+    "text/text11.webp",
+    "text/text12.webp",
+    "text/text13.webp",
+    "text/text14.webp",
+    "text/text15.webp",
+    "text/text16.webp",
+    "text/text17.webp",
+    "text/text18.webp",
+    "text/text19.webp",
+    "text/text20.webp",
+    "text/text21.webp",
+    "text/text22.webp",
+    "text/text23.webp",
+    "text/text24.webp",
+    "text/text25.webp",
+    "text/text26.webp",
+    "text/text27.webp",
+    "text/text28.webp",
 ];
 
 let currentIndex = 0;
 let shuffledIndices = [];
 
-const questionPhotoElement = document.getElementById("question-photo");
-const answerPhotoElement = document.getElementById("answer-photo");
-const showAnswerButton = document.getElementById("show-answer-button");
-const nextButton = document.getElementById("next-button");
-const resetButton = document.getElementById("reset-button");
-const questionCountElement = document.getElementById("question-count");
-
-document.getElementById("toggle-mode-button").addEventListener("click", toggleMode);
-document.getElementById("back-button").addEventListener("click", goBack);
-document.getElementById("start-button").addEventListener("click", start);
-document.getElementById("show-answer-button").addEventListener("click", showAnswer);
-document.getElementById("next-button").addEventListener("click", next);
-document.getElementById("reset-button").addEventListener("click", reset);
-
 function start() {
-    const startButton = document.getElementById("start-button");
-    startButton.classList.add("hidden");
-
-    questionPhotoElement.classList.remove("hidden");
-    showAnswerButton.classList.remove("hidden");
-    questionCountElement.classList.remove("hidden");
+    document.getElementById("start-button").classList.add("hidden");
+    document.getElementById("question-photo").classList.remove("hidden");
+    document.getElementById("show-answer-button").classList.remove("hidden");
+    document.getElementById("question-count").classList.remove("hidden");
 
     if (shuffledIndices.length === 0) {
         shuffledIndices = shuffleArray(questionPhotos.length);
     }
 
-    questionPhotoElement.src = questionPhotos[shuffledIndices[currentIndex]];
+    document.getElementById("question-photo").src = questionPhotos[shuffledIndices[currentIndex]];
     updateQuestionCount();
 }
 
 function updateQuestionCount() {
+    const questionCountElement = document.getElementById("question-count");
     questionCountElement.textContent = `Question ${currentIndex + 1} of ${questionPhotos.length}`;
 }
 
@@ -107,39 +92,39 @@ function shuffleArray(length) {
 }
 
 function showAnswer() {
-    answerPhotoElement.classList.remove("hidden");
-    showAnswerButton.classList.add("hidden");
-    nextButton.classList.remove("hidden");
-    answerPhotoElement.src = answerPhotos[shuffledIndices[currentIndex]];
+    document.getElementById("answer-photo").classList.remove("hidden");
+    document.getElementById("next-button").classList.remove("hidden");
+    document.getElementById("show-answer-button").classList.add("hidden");
+    document.getElementById("answer-photo").src = answerPhotos[shuffledIndices[currentIndex]];
 }
 
 function next() {
     currentIndex++;
     if (currentIndex < questionPhotos.length) {
-        questionPhotoElement.src = questionPhotos[shuffledIndices[currentIndex]];
-        answerPhotoElement.src = "";
-        showAnswerButton.classList.remove("hidden");
-        nextButton.classList.add("hidden");
-        resetButton.classList.add("hidden");
+        document.getElementById("question-photo").src = questionPhotos[shuffledIndices[currentIndex]];
+        document.getElementById("answer-photo").src = "";
+        document.getElementById("show-answer-button").classList.remove("hidden");
+        document.getElementById("next-button").classList.add("hidden");
+        document.getElementById("reset-button").classList.add("hidden");
         updateQuestionCount();
     } else {
-        questionPhotoElement.classList.add("hidden");
-        answerPhotoElement.classList.add("hidden");
-        showAnswerButton.classList.add("hidden");
-        nextButton.classList.add("hidden");
-        resetButton.classList.remove("hidden");
+        document.getElementById("question-photo").classList.add("hidden");
+        document.getElementById("answer-photo").classList.add("hidden");
+        document.getElementById("show-answer-button").classList.add("hidden");
+        document.getElementById("next-button").classList.add("hidden");
+        document.getElementById("reset-button").classList.remove("hidden");
     }
 }
 
 function reset() {
     currentIndex = 0;
     shuffledIndices = shuffleArray(questionPhotos.length);
-    questionPhotoElement.classList.remove("hidden");
-    answerPhotoElement.classList.add("hidden");
-    showAnswerButton.classList.remove("hidden");
-    nextButton.classList.add("hidden");
-    resetButton.classList.add("hidden");
-    questionPhotoElement.src = questionPhotos[shuffledIndices[currentIndex]];
+    document.getElementById("question-photo").classList.remove("hidden");
+    document.getElementById("answer-photo").classList.add("hidden");
+    document.getElementById("show-answer-button").classList.remove("hidden");
+    document.getElementById("next-button").classList.add("hidden");
+    document.getElementById("reset-button").classList.add("hidden");
+    document.getElementById("question-photo").src = questionPhotos[shuffledIndices[currentIndex]];
 }
 
 function toggleMode() {
@@ -160,13 +145,5 @@ function goBack() {
     window.location.href = "../KumNus/option.html";
 }
 
-function preloadImages(images) {
-    images.forEach((image) => {
-        const img = new Image();
-        img.src = image;
-    });
-}
 
-preloadImages(questionPhotos);
-preloadImages(answerPhotos);
 
